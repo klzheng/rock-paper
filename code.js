fc7c8a0cc5dc91c740abc9a89ddf3e4d1d7e360a
klzheng <klzheng@buffalo.edu>
2022-08-14 11:39:02 -0400

    Added documentation and cleaned up code; Added a scoreboard counter to console.log

--- code.js
+++ code.js
@@ -1,8 +1,13 @@
+// Generates random choice for computer player
+// @return {string} - computer's choice
 function getComputerChoice() {
     choices = ["Rock", "Paper", "Scissors"]
     return choices[Math.round(Math.random()*2)].toLowerCase()
 }
 
+
+// Asks and checks player's input
+// @return {string} - player's input
 function checkPlayerChoice() {
     let choice = prompt("Please choose rock, paper, or scissors").toLowerCase()
     while (choice != "rock" && choice != "paper" && choice != "scissors") {
@@ -11,6 +16,11 @@ function checkPlayerChoice() {
     return choice
 }
 
+
+// Plays a round of rock-paper-scissors
+// @param {string} playerSelection - player's choice
+// @param {string} computerSelection - computer's choice
+// @return {string} - notifies who won the round
 function playRound(playerSelection, computerSelection) {
     let losePhrase = `You lose, ${computerSelection} beats ${playerSelection}`
     let winPhrase = `You win, ${playerSelection} beats ${computerSelection}`
@@ -18,29 +28,35 @@ function playRound(playerSelection, computerSelection) {
 
     switch (true) {
         case (playerSelection == computerSelection):
-            console.log(tiePhrase)
+            // console.log(tiePhrase)
             return tiePhrase
             break
         case (playerSelection == "rock" && computerSelection == "paper"):
-            console.log(losePhrase)
+            // console.log(losePhrase)
             return losePhrase
             break
         case (playerSelection == "paper" && computerSelection == "scissor"):
-            console.log(losePhrase)
+            // console.log(losePhrase)
             return losePhrase
             break
         case (playerSelection == "scissors" && computerSelection == "rock"):
-            console.log(losePhrase)
+            // console.log(losePhrase)
             return losePhrase
             break
         default:
-            console.log(winPhrase)
+            // console.log(winPhrase)
             return winPhrase
     }
 }
 
+
+// Play rounds until a certain score is reached for either player
+// @return {string} - Tells who won the game after the score limit is reached
 function game() {
-    while (computerScore < 5 && playerScore < 5) {
+    let computerScore = 0;
+    let playerScore = 0;
+    const scoreLimit = 5;
+    while (computerScore < scoreLimit && playerScore < scoreLimit) {
         let playerChoice = checkPlayerChoice()
         let computerChoice = getComputerChoice()
 
@@ -49,18 +65,18 @@ function game() {
         } else if (playRound(playerChoice, computerChoice).includes("lose")) {
             computerScore ++
         } 
+        console.log(`Player Score: ${playerScore} \nComputer Score: ${computerScore}`)
     }
 
     switch (true) {
-        case (computerScore == 5):
+        case (computerScore == scoreLimit):
+            console.log("Computer Wins")
             return "Computer wins"
             break
         default:
+            console.log("Player Wins")
             return "Player wins"
     }
 }
 
-let computerScore = 0;
-let playerScore = 0;
-
-game()
+// game() <- type this in console to initiate game
